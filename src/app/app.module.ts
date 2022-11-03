134d1fe6d92ecf30f0c5fda42f5fbb7e11ed6c3b
Kaleo-Stark <kaleovieira.dev@gmail.com>
2022-11-02 22:12:22 -0300

    Base do projeto em Angular, Criado estrutura, header e Footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from '../componentes/topo/topo.component';
+import { RodapeComponent } from '../componentes/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
