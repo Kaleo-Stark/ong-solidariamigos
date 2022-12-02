b017d3f1d5cdea8fd08d3c9250edde948bb9408e
Kaleo-Stark <kaleovieira.dev@gmail.com>
2022-12-02 12:05:10 -0300

    Componente e conteúdo da seção doações

--- src/componentes/doacao/doacao.component.ts
+++ src/componentes/doacao/doacao.component.ts
@@ -12,19 +12,19 @@ export class DoacaoComponent implements OnInit {
   
   public imagensCarousel : Array<FotoService> = [
     {
-      src: `${this.caminhoDasImagens}/foto-1.webp`,
+      src: `${this.caminhoDasImagens}/imagem-1.png`,
       titulo: 'First slide label',
       sobre: 'Some representative placeholder content for the first slide.',
       ativo: true
     },
     {
-      src: `${this.caminhoDasImagens}/foto-2.webp`,
+      src: `${this.caminhoDasImagens}/imagem-2.png`,
       titulo: 'Second slide label',
       sobre: 'Some representative placeholder content for the second slide.',
       ativo: false
     },
     {
-      src: `${this.caminhoDasImagens}/foto-3.webp`,
+      src: `${this.caminhoDasImagens}/imagem-3.png`,
       titulo: 'Third slide label',
       sobre: 'Some representative placeholder content for the third slide.',
       ativo: false
