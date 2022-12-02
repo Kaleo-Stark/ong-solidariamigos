0888157a049e4ffc7dcf091a9397810b698836f7
Kaleo-Stark <kaleovieira.dev@gmail.com>
2022-12-02 10:10:50 -0300

    Componente e conteúdo da seção padrinho nota dez

---
+++ src/componentes/bazar/bazar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bazar',
+  templateUrl: './bazar.component.html',
+  styleUrls: ['./bazar.component.css']
+})
+export class BazarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
